684bea8837f56b8d7b3a504cb9ad24298744b2a0
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-24 13:21:16 +0200

    ApiAdded

--- DeviceApp/Program.cs
+++ DeviceApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
 using System;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using SharedLibrary.Models;
+using static SharedLibrary.Models.TemperatureApiModel;
 
 namespace DeviceApp
 {
@@ -11,61 +14,88 @@ namespace DeviceApp
 
         private static DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=ec-win20iothub.azure-devices.net;DeviceId=DeviceApp;SharedAccessKey=sJGB59/d4EwPyNVxsX/VXWzxQoZkivpeYQUN+Bu7j+k=");
         private static int telemetryInterval = 5;
-        private static Random rnd = new Random();
+        
 
         static void Main(string[] args)
         {
-            deviceClient.SetMethodHandlerAsync("SetTelementryInterval", SetTelemetryInterval, null).Wait();
+            deviceClient.SetMethodHandlerAsync("SetTelemetryInterval", SetTelemetryInterval, null).Wait();
             SendMessageAsync().GetAwaiter();
+
             Console.ReadKey();
         }
 
-        private static Task<MethodResponse> SetTelemetryInterval(MethodRequest request, object userContext) 
+        private static Task<MethodResponse> SetTelemetryInterval(MethodRequest request, object userContext)
         {
-            var payload = Encoding.UTF8.GetString(request.Data).Replace("\"","");
-            
-
+            var payload = Encoding.UTF8.GetString(request.Data).Replace("\"", "");
 
             if (Int32.TryParse(payload, out telemetryInterval))
             {
                 Console.WriteLine($"Interval set to: {telemetryInterval} seconds.");
 
+                // { "result": "Executed direct method: SetTelemetryInterval" }
                 string json = "{\"result\": \"Executed direct method: " + request.Name + "\"}";
-
                 return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 200));
             }
             else
             {
-                string json = "{\"result\": \"Invalid method\"}";
-
-                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 400));
+                string json = "{\"result\": \"Method not implemented\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 501));
             }
-        
         }
 
-        private static async Task SendMessageAsync() 
+        private static async Task SendMessageAsync()
         {
+            var httpClient = HttpClientFactory.Create();
+
             while (true)
             {
-                double temp = 10 + rnd.NextDouble() * 15;
-                double hum = 40 + rnd.NextDouble() * 20;
-                var data = new
+                double temp = 0;
+                int humidity = 0;
+                TemperatureModel senddata = new TemperatureModel();
+
+                try
                 {
-                    temperature = temp,
-                    humidity = hum
-                };
+                    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=59.27412&units=metric&lon=15.2066&exclude=hourly,daily,minutely&appid=5bf919005c4c20e778ba98f74c7f2e33";
+                    HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
 
-                var json = JsonConvert.SerializeObject(data);
-                var payload = new Message(Encoding.UTF8.GetBytes(json));
-                payload.Properties.Add("temperatureAlert", (temp > 30) ? "true" : "false");
+                    if (httpResponseMessage.IsSuccessStatusCode)
+                    {
+                        var content = httpResponseMessage.Content;                                    //Hämtar Api
+                        var datatemp = await content.ReadAsAsync<Rootobject>();
 
-                await deviceClient.SendEventAsync(payload);
-                Console.WriteLine($"Message sent: {json}");
+                        temp = datatemp.current.temp;                                                // Sätter Temp och Humidity
+                        humidity = datatemp.current.humidity;
+
+                        senddata = new TemperatureModel                                              //Lägger Temp och Humidity i samma objekt.
+                        {
+                            Temperature = temp,
+                            Humidity = humidity
+                        };
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                try
+                {
+                    var json = JsonConvert.SerializeObject(senddata);                                        // Converterar till Json format.
+
+                    var payload = new Message(Encoding.UTF8.GetBytes(json));                             // Packeterar meddelandena
+
+                    await deviceClient.SendEventAsync(payload);                                         // Skickar
+
+                    Console.WriteLine($"Message Sent: {json}");
+                }
+                catch (Exception exx)
+                {
+                    Console.WriteLine(exx.Message);
+                }
+
                 await Task.Delay(telemetryInterval * 1000);
-
             }
-            
-        
         }
     }
 }
--- ServiceApp/Program.cs
+++ ServiceApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices;
+using System;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -6,18 +7,25 @@ namespace ServiceApp
 {
     class Program
     {
-        private static ServiceClient serviceClient = ServiceClient.CreateFromConnectionString("");
+        private static ServiceClient serviceClient = ServiceClient.CreateFromConnectionString("HostName=ec-win20iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=gpHmAu5J/VlUZ63wn3YdwS4YHa9jpS/1ztgSFZmBd7k=");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task.Delay(5000).Wait();
+
+            InvokeMethod("DeviceApp", "SetTelemetryInterval", "2").GetAwaiter();
+            Console.ReadKey();
         }
 
-        static async Task InvokeMetod() 
+        static async Task InvokeMethod(string deviceId, string methodName, string payload)
         {
-         var methodInvocation = new
-         Console.WriteLine($"Response status: {}");
-        
-        
+            var methodInvocation = new CloudToDeviceMethod(methodName) { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            methodInvocation.SetPayloadJson(payload);
+
+
+            var response = await serviceClient.InvokeDeviceMethodAsync(deviceId, methodInvocation);
+            Console.WriteLine($"Response Status: {response.Status}");
+            Console.WriteLine(response.GetPayloadAsJson());
+
         }
     }
 }
