2bdf0c22c648532ac732f01111a1f63643ef4bd6
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-10-02 16:51:05 +0200

    AbsolutFinal

--- ServiceApp.Test/InvokeMethod.Tests.cs
+++ ServiceApp.Test/InvokeMethod.Tests.cs
@@ -1,5 +1,7 @@
 using System;
 using Xunit;
+using SharedLibrary.Services;
+
 
 namespace ServiceApp.Test
 {
@@ -15,5 +17,20 @@ namespace ServiceApp.Test
             Int32.TryParse(payload, out actual);
             Assert.Equal(expected, actual);
         }
+
+                         
+        [Theory]
+        [InlineData("DeviceApp", "SetTelemetryInterval", "5", "200")]
+        [InlineData("DeviceApp", "GetTelemetryInterval", "5", "501")]
+
+        public void ShouldGetResponsMessage(string targetDevice, string methodName, string payload, string expected) 
+        {
+            var service = new ServiceClientService("HostName=ec-win20iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=gpHmAu5J/VlUZ63wn3YdwS4YHa9jpS/1ztgSFZmBd7k=");
+            var response = service.InvokeMetodAsync(targetDevice, methodName, payload);
+
+            Assert.Equal(expected, response.Result.Status.ToString());
+        }
+
+
     }
 }
---
+++ SharedLibrary/Services/ServiceClientService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Devices;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Services
+{
+    public class ServiceClientService
+    {
+        private ServiceClient serviceClient;
+
+        public ServiceClientService(string connectionstring) 
+        {
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionstring);
+        }
+
+        public async Task<CloudToDeviceMethodResult> InvokeMetodAsync(string targetDevice, string metodName, string payload) 
+        {
+            var metodInvocation = new CloudToDeviceMethod(metodName);
+            metodInvocation.SetPayloadJson(payload);
+
+            var response = await serviceClient.InvokeDeviceMethodAsync(targetDevice, metodInvocation);
+            return response;
+        
+        }
+
+
+
+
+
+    }
+}
